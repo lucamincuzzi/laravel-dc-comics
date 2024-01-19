00b219859a425f06925acb36c2415db3dfe7cdd0
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-01-19 16:23:05 +0100

    update: mass assingment implemented for store method in ComicController

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -40,13 +40,7 @@ class ComicController extends Controller
         $form_data = $request->all();
 
         $comic = new Comic();
-        $comic->title = $form_data['title'];
-        $comic->description = $form_data['description'];
-        $comic->thumb =  $form_data['thumb'];
-        $comic->price = $form_data['price'];
-        $comic->series = $form_data['series'];
-        $comic->sale_date = $form_data['sale_date'];
-        $comic->type = $form_data['type'];
+        $comic->fill($form_data);
 
         $comic->save();
 
@@ -72,7 +66,7 @@ class ComicController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
-    {
+    {   
         //
     }
 
